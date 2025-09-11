1da8c6c00efb945b457b5967a4b901114aa56c3c
podechardt <podechardt@SNIR-P133>
2025-09-11 08:41:40 +0200

    #2 Serveur Web

--- JeuxJS/app.js
+++ JeuxJS/app.js
@@ -1,3 +1,18 @@
-'use strict';
+// eslint.config.cjs 
+const globals = require("globals");
+const pluginJs = require("@eslint/js");
 
-console.log('Hello world');
+module.exports = [
+    {
+        languageOptions: {
+            globals: globals.browser,
+        },
+        rules: {
+            "indent": ["warn", 4],          // avertir une indentation de 4 espaces 
+            "linebreak-style": ["error", "windows"], // impose les fins de ligne Windows (\r\n) 
+            "quotes": ["error", "single"],   // impose l'usage de guillemets simples (' ') 
+            "semi": ["error", "always"]      // impose le point-virgule obligatoire 
+        },
+    },
+    pluginJs.configs.recommended,
+]; 
--- JeuxJS/eslint.config.js
+++ JeuxJS/eslint.config.js
@@ -1,7 +1,51 @@
-module.exports = [
-    {
-        rules: {
-            // Add rules here.
-        }
-    }
-];
+/*  *************** Serveur Web *********************   */
+//
+'use strict';
+
+console.log('TP CIEL');
+
+var port = 80;
+
+var express = require('express');
+var exp = express();
+exp.use(express.static(__dirname + '/www'));
+
+exp.get('/', function (req, res) {
+    console.log('Reponse a un client');
+    res.sendFile(__dirname + '/www/index.html');
+});
+exp.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.status(500).send('Erreur serveur express');
+});
+exp.listen(port, function () {
+    console.log('Serveur en ecoute');
+});
+
+/*  *************** Serveur WebSocket express *********************   */
+// 
+var expressWs = require('express-ws')(exp);
+
+// Connexion des clients à la WebSocket /echo et evenements associés 
+exp.ws('/echo', function (ws, req) {
+    console.log('Connection WebSocket %s sur le port %s',
+        req.connection.remoteAddress, req.connection.remotePort);
+
+    ws.on('message', function (message) {
+        console.log('De %s %s, message :%s', req.connection.remoteAddress,
+            req.connection.remotePort, message);
+        ws.send(message);
+    });
+
+    ws.on('close', function (reasonCode, description) {
+        console.log('Deconnexion WebSocket %s sur le port %s',
+            req.connection.remoteAddress, req.connection.remotePort);
+    });
+});
+
+/*  ****** Serveur Web et WebSocket en écoute sur le port 80  ********   */
+//  
+var portServ = 80;
+exp.listen(portServ, function () {
+    console.log('Serveur en ecoute');
+}); 
